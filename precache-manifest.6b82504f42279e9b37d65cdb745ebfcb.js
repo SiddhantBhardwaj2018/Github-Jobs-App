d69323195f9e84a42c7b562e93972709cf3d79a9
SiddhantBhardwaj2018 <bhardwajsiddhant03@gmail.com>
2020-10-24 14:13:16 -0700

    Updates

---
+++ precache-manifest.6b82504f42279e9b37d65cdb745ebfcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d975ffe14964d880ed97099225366f4b",
+    "url": "/Github-Jobs-App/index.html"
+  },
+  {
+    "revision": "f60fb4293235c5040540",
+    "url": "/Github-Jobs-App/static/css/main.de7dd0fe.chunk.css"
+  },
+  {
+    "revision": "5cd0aaf607b8a4c56ef5",
+    "url": "/Github-Jobs-App/static/js/2.9a37bce8.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Github-Jobs-App/static/js/2.9a37bce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60fb4293235c5040540",
+    "url": "/Github-Jobs-App/static/js/main.23406365.chunk.js"
+  },
+  {
+    "revision": "d49a3fffcf834ee8ceeb",
+    "url": "/Github-Jobs-App/static/js/runtime-main.c210e106.js"
+  }
+]);
